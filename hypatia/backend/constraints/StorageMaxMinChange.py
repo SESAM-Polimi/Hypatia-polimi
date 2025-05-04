1494b1e286a82adaae71edce42b36a2dc9b2bcd6
tomzac19 <tommasozacconi1998@gmail.com>
2025-05-04 10:57:10 +0200

    Update StorageMaxMinChange constraint
    Due to the missing lines, such constraint didn't allow the model to properly size the storage!

--- hypatia/backend/constraints/StorageMaxMinChange.py
+++ hypatia/backend/constraints/StorageMaxMinChange.py
@@ -12,30 +12,34 @@ class StorageMaxMinChange(Constraint):
         assert hasattr(self.variables, 'totalcapacity'), "totalcapacity must be defined"
 
     def rules(self):
+        timeslice_fraction = self.model_data.settings.timeslice_fraction
+        if not isinstance(timeslice_fraction, int):
+            timeslice_fraction.shape = (len(self.model_data.settings.time_steps), 1)
+            
         rules = []
         for reg in get_regions_with_storage(self.model_data.settings):
             for indx, year in enumerate(self.model_data.settings.years):
                 
+                annual_storage_capacity = cp.multiply(
+                    self.variables.totalcapacity[reg]["Storage"][indx : indx + 1, :],
+                    timeslice_fraction) * 8760
+                
                 rules.append(
-                    self.variables.totalcapacity[reg]["Storage"][indx : indx + 1, :]
+                    annual_storage_capacity
                     - self.variables.storage_SOC[reg][
-                        indx
-                        * len(self.model_data.settings.time_steps) : (indx + 1)
-                        * len(self.model_data.settings.time_steps),
-                        :,
-                    ]
+                        indx* len(self.model_data.settings.time_steps)
+                        : (indx + 1) * len(self.model_data.settings.time_steps),
+                        :]
                     >= 0
-                )
+                )                
                 
                 rules.append(
                     self.variables.storage_SOC[reg][
-                        indx
-                        * len(self.model_data.settings.time_steps) : (indx + 1)
-                        * len(self.model_data.settings.time_steps),
-                        :,
-                    ]
+                        indx * len(self.model_data.settings.time_steps) 
+                        : (indx + 1) * len(self.model_data.settings.time_steps),
+                        :]
                     - cp.multiply(
-                        self.variables.totalcapacity[reg]["Storage"][indx : indx + 1, :],
+                        annual_storage_capacity,
                         self.model_data.regional_parameters[reg]["storage_min_SOC"].values[indx : indx + 1, :]
                     )
                     >= 0
