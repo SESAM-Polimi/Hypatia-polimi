666a971515e361a0e75152052278d5e230c0f0ec
Nicolò Stevanato <nicolo.stevanato@polimi.it>
2025-05-06 17:29:03 +0300

    removed MILP typo from param_global

--- hypatia/backend/ModelSettings.py
+++ hypatia/backend/ModelSettings.py
@@ -273,13 +273,7 @@ class ModelSettings:
                         "value": 0.05,
                         "index": pd.Index(self.years, name="Years"),
                         "columns": pd.Index(["Annual Discount Rate"]),
-                    },
-                "global_new_capacity_step": {
-                        "sheet_name": "Modular_cap_unit",
-                        "value": 0,
-                        "index": pd.Index(["Step capacity increase"], name='Parameter'),
-                        "columns": indexer_global,
-                    },
+                    }
                 }
             )
 
