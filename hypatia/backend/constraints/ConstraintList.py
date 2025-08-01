8089ec4a2f80a56da8a5bff27b0228b158d098de
Tommaso Zacconi <tommasozacconi1998@gmail.com>
2025-08-01 11:50:04 +0200

    Useful comments added in ConstraintList

--- hypatia/backend/constraints/ConstraintList.py
+++ hypatia/backend/constraints/ConstraintList.py
@@ -20,11 +20,12 @@ from hypatia.backend.constraints.LineAvailability import LineAvailability
 from hypatia.backend.constraints.LineNewCapacity import LineNewCapacity
 from hypatia.backend.constraints.LandUsageRegional import LandUsageRegional
 from hypatia.backend.constraints.LandUsageGlobal import LandUsageGlobal
-# from hypatia.backend.constraints.ElectrolysisConsumption import ElectrolysisConsumption # To make it work, "Electricity" carrier must be explicitly specified
 from hypatia.backend.constraints.RenewableProductionRegional import RenewableProductionRegional
 from hypatia.backend.constraints.RenewableProductionGlobal import RenewableProductionGlobal  
-#from hypatia.backend.constraints.NewCapacityNonneg import NewCapacityNonneg
-#from hypatia.backend.constraints.ProductionRamp import ProductionRamp
+# from hypatia.backend.constraints.ElectrolysisConsumption import ElectrolysisConsumption # To make it work, "Electricity" carrier must be explicitly specified
+
+#from hypatia.backend.constraints.NewCapacityNonneg import NewCapacityNonneg # This constraint is functional to MILP version, but it is not implemented in the current model, so it is not included in the list of constraints.
+#from hypatia.backend.constraints.ProductionRamp import ProductionRamp # The current definition of ProductionRamp is not compatible with the effects brought by the StorageCyclciBoundary constraint, so it is not included in the list of constraints.
 
 CONSTRAINTS = [
     Balance,
@@ -48,11 +49,9 @@ CONSTRAINTS = [
     RenewableProductionGlobal,
     LineAvailability,
     LineNewCapacity,
-#    NewCapacityNonneg,
     RenewableProductionRegional,
     LandUsageRegional,
     LandUsageGlobal,
-    # ProductionRamp,
     # ElectrolysisConsumption
 ]
 
